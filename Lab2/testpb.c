bae0e13d2f822f0dc31bae9e6efd7c164af14dca
Kira <kirak5683@gmail.com>
2022-10-12 20:43:31 -0700

    Updated debouncing code following Ganssele's example of using an array to store button states. Now all push buttons can be debounced simultaneously

--- Lab2/testpb.c
+++ Lab2/testpb.c
@@ -11,16 +11,17 @@
 //*******************************************************************************
 //                            debounce_switch                                  
 // Adapted from Ganssel's "Guide to Debouncing"            
-// Checks the state of pushbutton S0 It shifts in ones till the button is pushed. 
-// Function returns a 1 only once per debounced button push so a debounce and toggle 
-// function can be implemented at the same time. Expects active low pushbutton on 
-// Port A. Change bit number in the "bit_is_clear" call to change which pushbutton
-// is tested. Debounce time is determined by external loop delay times 12. 
+// Similar to the code for the single pushbutton debounce, except for this code
+// operates on an array of saved pushbutton states as outlined in Ganssel's code.
+// Each time this function is called, ones are shifted into an index in the state
+// array at the button index passed to this function. If the value at that index 
+// is 0xF000 after 1s shifting, return 1. This stays in the loop for 12 cycles 
+// times a delay in main to get a time delay in ms.
 //*******************************************************************************
-int8_t debounce() {
-  static uint16_t state = 0; //holds present state
-  state = (state << 1) | (! bit_is_clear(PINA, 0)) | 0xE000;
-  if (state == 0xF000) return 1;
+int8_t debounce(uint8_t bidx) {
+  static uint16_t pbstate[8] = {0};  //
+  pbstate[bidx] = (pbstate[bidx] << 1) | (! bit_is_clear(PINA, bidx)) | 0xE000;
+  if(pbstate[bidx] == 0xF000) return 1;
   return 0;
 }
 
@@ -34,9 +35,11 @@ DDRB = 0xFF;  //set port B to all outputs
 DDRA = 0x00;  //set port A to all inputs
 PORTA = 0xFF; //set port A to all pullups
 while(1){     //do forever
- if(debounce()) { //if switch true for 12 passes, increment port B
+ for(int i = 0; i < 8; i++){
+     if(debounce(i)) { //if switch true for 12 passes, increment port B
         PORTB++;
- }
+     } //if
+  } // for
   _delay_ms(2);                    //keep in loop to debounce 24ms 
  } //while 
 } //main
