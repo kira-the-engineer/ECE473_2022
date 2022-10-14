881ef92157b0da2ca34cd5e88e6c1042ef0fc4f5
Kira <kirak5683@gmail.com>
2022-10-13 18:26:17 -0700

    Finished writing segsum function: it has not been tested

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -15,10 +15,24 @@
 #include <util/delay.h>
 
 //holds data to be sent to the segments. logic zero turns segment on
-uint8_t segment_data[5] 
+uint8_t segment_data[5]; 
 
 //decimal to 7-segment LED display encodings, logic "0" turns on segment
-uint8_t dec_to_7seg[12] 
+//used in the decimal to segment encoding function
+uint8_t dec_to_7seg[12] = {
+       0b11000000, // display 0
+       0b11111001, // display 1
+       0b10100100, // display 2
+       0b10110000, // display 3
+       0b10011001, // display 4
+       0b10010010, // display 5
+       0b10000010, // display 6 
+       0b11111000, // display 7
+       0b10000000, // display 8
+       0b10010000, // display 9
+       0b01111111, // display . 
+       0b11111111, // clear all segments (active low)
+};
 
 
 //******************************************************************************
@@ -44,9 +58,39 @@ uint8_t chk_buttons(uint8_t button) {
 //array is loaded at exit as:  |digit3|digit2|colon|digit1|digit0|
 void segsum(uint16_t sum) {
   //determine how many digits there are 
+  uint8_t dig_cnt = 0; 
+  if(sum < 10) dig_cnt = 1; // checks if sum is 0 through 9
+  else if(sum < 100 && sum >= 10) dig_cnt = 2; // checks if sum is 10 through 99
+  else if(sum < 1000 && sum >= 100) dig_cnt = 3; // checks if sum is 1000 through 999
+  else if(sum <= 1023) dig_cnt = 4; // checks if sum is 1000 through 1023
+  else {
+       dig_cnt = 0; // if sum is greater than 1023 or error
+  }
+
   //break up decimal sum into 4 digit-segments
-  //blank out leading zero digits 
-  //now move data to right place for misplaced colon position
+  //create integers to break sum value into ones, tens, hundreds, thousands place
+  uint8_t ones = (sum % 10);
+  uint8_t tens = ((sum / 10) % 10);
+  uint8_t hundreds = ((sum / 100) % 10);
+  uint8_t thousands = ((sum / 1000) % 10);
+
+  //load encoded values into segment data array
+  segment_data[0] = dec_to_7seg[ones]; // encode digit for ones place
+  segment_data[1] = dec_to_7seg[tens]; // encode digit for tens place
+  segment_data[2] = 0xFF; // Keep colon off
+  segment_data[3] = dec_to_7seg[hundreds]; // encode digit for hundreds place
+  segment_data[4] = dec_to_7seg[thousands]; // encode digit for thousands place
+
+
+  //blank out leading zero digits
+  switch(dig_cnt){
+	case 4: break; // if there's 4 digits, don't blank anything and break
+	case 3: segment_data[4] = 0xFF; break; // if there's 3 digits, blank the thou place
+	case 2: segment_data[4] = 0xFF; segment_data[3] = 0xFF; break; // if there's 2 digits, blank thou, hund place 
+	case 1: segment_data[4] = 0xFF; segment_data[3] = 0xFF; segment_data[1] = 0xFF; break; // if there's 1 digit blank thou, hund, tens place
+	//just display a single zero if 0 or over 1023
+	default: segment_data[4] = 0xFF; segment_data[3] = 0xFF; segment_data[1] = 0xFF; segment_data[0] = 0x00; break; 
+  }
 }//segment_sum
 //***********************************************************************************
 
@@ -67,19 +111,20 @@ while(1){
   // buffer on the pushbutton board, and drives the enable pin
   for(int i = 0; i < 8; i++){ // iterate over buttons on PORTA 
 	if(chk_buttons(i)) {
-		seg_sum += (1 << i) // Use binary shifting of 0b00000000 to make 
-				    // each switch increment the value of sum by a power of 2
+		seg_sum += (1 << i); // Use binary shifting of 0b00000000 to make 
+				     // each switch increment the value of sum by a power of 2
 	}
   }
-  //disable tristate buffer for pushbutton switches by clearing bit 6, sending bit 8 low
+  //disable tristate buffer for pushbutton switches by clearing bit 6, sending bit 8 of the decoder low
   PORTB &= ~(1<<PB6);
   //bound the count to 0 - 1023
   if(seg_sum > 1023) {
 	seg_sum = 0;
   }
   //break up the disp_value to 4, BCD digits in the array: call (segsum)
-  //bound a counter (0-4) to keep track of digit to display 
+  segsum(seg_sum); 
   //make PORTA an output
+  DDRA = 0xFF;
   //send 7 segment code to LED segments
   //send PORTB the digit to display
   //update digit to display
